bc17e7c559fda569593320de9c5e775951427e6f
Florian Apolloner <florian@apolloner.eu>
2013-03-31 23:28:21 +0200

    decoupled the js a bit to allow testing for post requests.
    Note: I have no idea of js, gotta rewrite that soon ;)

--- ajax_blocks/static/ajax_blocks/helpers.js
+++ ajax_blocks/static/ajax_blocks/helpers.js
@@ -1,4 +1,6 @@
 var render_ajax_blocks;
+var ajax_blocks_callback;
+
 (function($) {
 	var initial_request_done = false;
 	function replace_blocks(data) {
@@ -7,25 +9,24 @@ var render_ajax_blocks;
 			elem.html(val);
 		});
 	}
-	render_ajax_blocks = function(url) {
-		$.ajax(url, {
-			success: function(data) {
-				if (!initial_request_done) {
-					if (typeof(history.replaceState) !== 'undefined') {
-						var state = {};
-						$.each(data.blocks, function(i, val) {
-							state[i] = $('*[data-block=' + i + ']:first').html();
-						});
-						history.replaceState(state, null, window.location);
-					}
-					initial_request_done = true;
-				}
-				replace_blocks(data.blocks)
-				if (typeof(history.pushState) !== 'undefined') {
-					history.pushState(data.blocks, null, data.url)
-				}
+	ajax_blocks_callback = function(data) {
+		if (!initial_request_done) {
+			if (typeof(history.replaceState) !== 'undefined') {
+				var state = {};
+				$.each(data.blocks, function(i, val) {
+					state[i] = $('*[data-block=' + i + ']:first').html();
+				});
+				history.replaceState(state, null, window.location);
 			}
-		});
+			initial_request_done = true;
+		}
+		replace_blocks(data.blocks)
+		if (typeof(history.pushState) !== 'undefined') {
+			history.pushState(data.blocks, null, data.url)
+		}
+	}
+	render_ajax_blocks = function(url) {
+		$.ajax(url, {success: ajax_blocks_callback});
 	}
 	window.onpopstate = function(event) {
 		replace_blocks(event.state);
