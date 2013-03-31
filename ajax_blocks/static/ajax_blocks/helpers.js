d9b9b833242553bc0bee7a409d02c6237f31c0a3
Florian Apolloner <florian@apolloner.eu>
2013-03-31 23:42:22 +0200

    Get rid of useless variables.

--- ajax_blocks/static/ajax_blocks/helpers.js
+++ ajax_blocks/static/ajax_blocks/helpers.js
@@ -2,7 +2,6 @@ var render_ajax_blocks;
 var ajax_blocks_callback;
 
 (function($) {
-	var initial_request_done = false;
 	function replace_blocks(data) {
 		$.each(data, function(i, val) {
 			var elem = $('*[data-block=' + i + ']:first');
@@ -10,7 +9,7 @@ var ajax_blocks_callback;
 		});
 	}
 	ajax_blocks_callback = function(data) {
-		if (!initial_request_done) {
+		if (!history.state) {
 			if (typeof(history.replaceState) !== 'undefined') {
 				var state = {};
 				$.each(data.blocks, function(i, val) {
@@ -18,7 +17,6 @@ var ajax_blocks_callback;
 				});
 				history.replaceState(state, null, window.location);
 			}
-			initial_request_done = true;
 		}
 		replace_blocks(data.blocks)
 		if (typeof(history.pushState) !== 'undefined') {
